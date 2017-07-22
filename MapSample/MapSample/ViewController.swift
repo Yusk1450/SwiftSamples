6a81c90c927359a898044f756bb92f2bbe01fa24
Yusk <ishigo12@iamas.ac.jp>
2017-07-22 19:28:09 +0900

    マップにピンを立てる機能の実装

--- MapSample/MapSample/ViewController.swift
+++ MapSample/MapSample/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController, CLLocationManagerDelegate
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate
 {
 	@IBOutlet weak var mapView: MKMapView!
 	var locationManager:CLLocationManager = CLLocationManager()
@@ -18,10 +18,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate
 	override func viewDidLoad()
 	{
 		super.viewDidLoad()
-		
-		self.mapView.setCenter(self.mapView.userLocation.coordinate, animated: true)
-		self.mapView.userTrackingMode = .followWithHeading
-		
+
+		// 現在地を取得する準備
 		if (CLLocationManager.locationServicesEnabled())
 		{
 			self.locationManager.delegate = self
@@ -29,7 +27,22 @@ class ViewController: UIViewController, CLLocationManagerDelegate
 			self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
 		}
 		
+		// マップの中心を現在地に設定する
+		self.mapView.setCenter(self.mapView.userLocation.coordinate, animated: true)
+		self.mapView.userTrackingMode = .followWithHeading
+
+		// 長押しを取得する準備
+		let longGesture = UILongPressGestureRecognizer()
+		longGesture.addTarget(self, action: #selector(self.longPress(sender:)))
+		self.mapView.addGestureRecognizer(longGesture)
 	}
+	
+	override func didReceiveMemoryWarning()
+	{
+		super.didReceiveMemoryWarning()
+	}
+
+	// MARK: - CLLocationManager Delegate Methods
 	
 	func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus)
 	{
@@ -50,10 +63,43 @@ class ViewController: UIViewController, CLLocationManagerDelegate
 	{
 	}
 	
-	override func didReceiveMemoryWarning()
+	// MARK: - UILongPressGestureRecognizer Methods
+	
+	func longPress(sender:UILongPressGestureRecognizer)
 	{
-		super.didReceiveMemoryWarning()
+		if (sender.state != .began)
+		{
+			return
+		}
+		
+		let location = sender.location(in: self.mapView)
+		let mapPoint = self.mapView.convert(location, toCoordinateFrom: self.mapView)
+		
+		let annotation = MKPointAnnotation()
+		annotation.coordinate = mapPoint
+		annotation.title = "ピンのタイトル"
+		annotation.subtitle = "ピンのサブタイトル"
+		self.mapView.addAnnotation(annotation)
 	}
+	
+	// MARK: - MKMapView Delegate Methods
+	
+	func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView?
+	{
+		let identifier = "PinAnnotationIdentifier"
+		
+		var pinView:MKPinAnnotationView? = self.mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
+
+		if (pinView == nil)
+		{
+			pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+			pinView?.animatesDrop = true
+		}
+		pinView?.annotation = annotation
+		
+		return pinView
+	}
+	
 
 }
 
